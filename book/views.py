6ef4805d8b97dd177493ee8aceafdbd557fa3f41
LukeGu <luke.gu.work@gmail.com.au>
2024-07-31 09:51:00 +1000

    feat: update book admin

--- book/views.py
+++ book/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework.authtoken.models import Token
 from .models import Book
 from .serializers import BookSerializer
 # from .permissions import IsOwnerOrReadonly, IsOwner
