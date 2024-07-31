d3e8c52f097dc766e7ceb03ba9562bd41c6d735f
LukeGu <luke.gu.work@gmail.com>
2024-07-31 10:05:35 +1000

    feat: update book admin

--- book/admin.py
+++ book/admin.py
@@ -4,6 +4,9 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'user', 'note')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 10
 
 
 # Register your models here.
