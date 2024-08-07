869f382bb3919ac4b65ea23ecc9e92262f39fea1
LukeGu <luke.gu.work@gmail.com>
2024-08-07 19:53:39 +1000

    feat: SMTP email setup

--- moneyplant/settings.py
+++ moneyplant/settings.py
@@ -174,3 +174,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
