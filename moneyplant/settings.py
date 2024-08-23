5b0dbc79e9c0fdbd88d672272b0a94c897b4425e
LukeGu <luke.gu.work@gmail.com>
2024-08-23 16:30:09 +1000

    feat: add throttling limit

--- moneyplant/settings.py
+++ moneyplant/settings.py
@@ -175,7 +175,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '20/minute'
+    }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
