63d9fc6b158a6c3cceac43674eed13962108a35b
LukeGu <luke.gu.work@gmail.com>
2024-08-27 16:47:40 +1000

    feat: add monthly goal in book

--- book/models.py
+++ book/models.py
@@ -6,6 +6,8 @@ class Book(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     note = models.CharField(max_length=500, blank=True, default='')
+    monthly_goal = models.DecimalField(
+        max_digits=12, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name
