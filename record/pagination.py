4b0f018301da44ab880f9394e0fc8a58e16fe373
LukeGu <luke.gu.work@gmail.com>
2025-01-03 22:43:15 +1100

    chore: increase record list page size

--- record/pagination.py
+++ record/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class RecordListCreatePagination(PageNumberPagination):
-    page_size = 35
+    page_size = 50
     page_size_query_param = "size"
     max_page_size = 100
